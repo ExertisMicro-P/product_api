bb6dc2f63ee28a7f18fc4d66a33b07fc022226ba
Noel Walsh <noel@crewe-it.co.uk>
2015-09-09 11:28:41 +0100

    Rearranged the form to handle a missing uploadPath setting - which seemed to be the case when I started

--- uploader/views/site/index.php
+++ uploader/views/site/index.php
@@ -12,57 +12,67 @@ $this->title = 'Product API Image uploader';
 ?>
 <div class="site-index">
 
+
     <div class="jumbotron">
         <h1>Product API</h1>
-
-        <p class="lead">Upload Product Image Files.</p>
-
+        <p class="lead text-center">Upload Product Image Files.</p>
+    </div>
+    <div>
         <?php
-        if (Yii::$app->user->isGuest) {
-            echo "<p>Please ".Html::a('Login', Url::toRoute('site/login'), ['class'=>'btn btn-primary'])." to use this uploader</p>";
+
+        if (($err = Yii::$app->session->getFlash('error')) <> '') {
+            echo '<div class="alert alert-danger">' . $err . '</div>';
+
+        } else {
+            ?>
+
+
+            <?php
+            if (Yii::$app->user->isGuest) {
+                echo "<p>Please " . Html::a('Login', Url::toRoute('site/login'), ['class' => 'btn btn-primary']) . " to use this uploader</p>";
+            }
+
+
+            if (!Yii::$app->user->isGuest) {
+
+                $form = ActiveForm::begin([
+                    'options' => ['enctype' => 'multipart/form-data'] // important
+                ]);
+                //echo $form->field($model, 'filename');
+
+                //// your fileinput widget for single file upload
+                //echo $form->field($model, 'filename')->widget(FileInput::classname(), [
+                //    'options'=>['accept'=>'image/*'],
+                //    'pluginOptions'=>[
+                //        'allowedFileExtensions'=>['jpg'/*,'gif','png'*/],
+                //        'mainClass' => 'input-group-lg'
+                //        ]
+                //]);
+
+                echo FileInput::widget([
+                    'model'     => $model,
+                    'attribute' => 'images[]',
+                    'options'   => ['multiple' => true]
+                ]);
+                /**
+                 * uncomment for multiple file upload
+                 * RCH 20141121 - couldn't get this to work, wouldn't populate the $_POST
+                 *
+                 *
+                 * echo $form->field($model, 'image[]')->widget(FileInput::classname(), [
+                 * 'options'=>['accept'=>'image/*', 'multiple'=>true],
+                 * 'pluginOptions'=>['allowedFileExtensions'=>['jpg','gif','png']
+                 * ]);
+                 *
+                 */
+                /*echo Html::submitButton('Upload' , [
+                    'class'=>'btn btn-success']
+                );
+                 *
+                 */
+                ActiveForm::end();
+
+            }
         }
-        ?>
-<?php
-
-if (!Yii::$app->user->isGuest) {
-
-$form = ActiveForm::begin([
-    'options'=>['enctype'=>'multipart/form-data'] // important
-]);
-//echo $form->field($model, 'filename');
-
-// your fileinput widget for single file upload
-echo $form->field($model, 'filename')->widget(FileInput::classname(), [
-    'options'=>['accept'=>'image/*'],
-    'pluginOptions'=>[
-        'allowedFileExtensions'=>['jpg'/*,'gif','png'*/],
-        'mainClass' => 'input-group-lg'
-        ]
-]);
-
-echo FileInput::widget([
-    'model' => $model,
-    'attribute' => 'images[]',
-    'options' => ['multiple' => true]
-]);
-/**
- * uncomment for multiple file upload
- * RCH 20141121 - couldn't get this to work, wouldn't populate the $_POST
- *
-
-echo $form->field($model, 'image[]')->widget(FileInput::classname(), [
-    'options'=>['accept'=>'image/*', 'multiple'=>true],
-    'pluginOptions'=>['allowedFileExtensions'=>['jpg','gif','png']
-]);
-*
-*/
-/*echo Html::submitButton('Upload' , [
-    'class'=>'btn btn-success']
-);
- *
- */
-ActiveForm::end();
-
-}
 ?>
 </div>

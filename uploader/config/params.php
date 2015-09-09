1e22887af9b2d12efc5014476d92c1699c292a58
Noel Walsh <noel@crewe-it.co.uk>
2015-09-09 11:26:33 +0100

    Corrected uploadPath setting

--- uploader/config/params.php
+++ uploader/config/params.php
@@ -3,5 +3,5 @@
 return [
     'adminEmail' => 'russell.hutson@exertis.co.uk',
 
-    'uploadPath' => realpath(dirname(__FILE__).'../../images/'), // '/var/www/html/exmicrosites/apps/product_api/uploads/', // RCH 20141120
+    'uploadPath' => realpath(dirname(__FILE__).'/../../images/'), // '/var/www/html/exmicrosites/apps/product_api/uploads/', // RCH 20141120
 ];
